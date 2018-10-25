b164f7da7bef026d3021da345850db762499c4c5
Bradley Barrows <bradleyb1537@gmail.com>
2018-10-25 08:50:35 -0700

    shaker

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// Looking to add filters from
+// https://cdn.rawgit.com/ktingvoar/PixiGlitch/0752d6628e23920a429da71a1881552b16a3b932/examples/dashboard/index.html
+// http://gun.net.au/
+
+// Square sin wave idea and code basically from the same guy
+
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
 
@@ -12,6 +18,11 @@ window.addEventListener("resize", function(event){
 var container = new PIXI.Container();
 
 app.stage.addChild(container);
+
+
+
+
+
 
 
 
@@ -55,6 +66,10 @@ for (var i=0; i<SQUARE_COUNT; i++) {
 
 
 
+
+
+
+
 var style = new PIXI.TextStyle({
     fontFamily: 'Arial',
     fontSize: 36,
@@ -77,6 +92,81 @@ richText.x = WIDTH/2 - 120;
 richText.y = 180;
 
 container.addChild(richText);
+
+
+
+
+
+
+
+
+let shakerFrag = `
+precision mediump float;
+uniform sampler2D uSampler;
+varying vec2 vTextureCoord;
+uniform vec2 blur;
+uniform vec4 dimensions;
+void main (void)
+{
+   vec4 col = texture2D(uSampler, vTextureCoord);
+   float pix_w = 1.0 / dimensions.x;
+   float pix_h = 1.0 / dimensions.y;
+   vec4 col_s[5], col_s2[5];
+   for (int i = 0;i < 5;i++){
+       col_s[i] = texture2D(uSampler, vTextureCoord + vec2(-pix_w * float(i) * blur.x, -pix_h * float(i) * blur.y));
+       col_s2[i] = texture2D(uSampler, vTextureCoord + vec2( pix_w * float(i) * blur.x, pix_h * float(i) * blur.y));
+   }
+   col_s[0] = (col_s[0] + col_s[1] + col_s[2] + col_s[3] + col_s[4])/5.0;
+   col_s2[0]= (col_s2[0] + col_s2[1] + col_s2[2] + col_s2[3] + col_s2[4])/5.0;
+   col = (col_s[0] + col_s2[0]) / 2.0;
+   gl_FragColor.rgba = col.rgba;
+}
+`;
+
+
+function ShakerFilter() {
+    PIXI.AbstractFilter.call(this,
+      
+      null,
+
+      shakerFrag,
+
+
+    {
+        dimensions: {type: '4fv', value: [0, 0, 0, 0]},
+        blur: {type: '2fv', value: [5, 0]}
+    }
+    );
+
+};
+
+ShakerFilter.prototype = Object.create(PIXI.AbstractFilter.prototype);
+ShakerFilter.prototype.constructor = ShakerFilter;
+
+Object.defineProperty(ShakerFilter.prototype, 'blurX', {
+    get: function() {
+        return this.uniforms.blur.value[0];
+    },
+    set: function(value) {
+        this.dirty = true;
+        this.uniforms.blur.value[0] = value;
+    }
+});
+
+Object.defineProperty(ShakerFilter.prototype, 'blurY', {
+    get: function() {
+        return this.uniforms.blur.value[1];
+    },
+    set: function(value) {
+        this.dirty = true;
+        this.uniforms.blur.value[1] = value;
+    }
+});
+
+
+let shakerFilter = new ShakerFilter();
+container.filters = [shakerFilter];
+
 
 
 
