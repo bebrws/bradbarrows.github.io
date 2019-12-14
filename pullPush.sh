d602974015740d5085c7166ced0f4889254403ee
Brad Barrows <bradebarrowsdev@gmail.com>
2019-12-13 22:21:06 -0800

    saving

--- pullPush.sh
+++ pullPush.sh
@@ -1,1 +1,5 @@
- git pull --rebase origin master; git push origin master
+cp CNAME public/CNAME
+cp CNAME docs/CNAME
+git add *
+git commit -am "saving"
+git pull --rebase origin master; git push origin master
