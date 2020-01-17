d4a0620eed036013b22e95dc7204c2e98f5c80e5
Brad Barrows <bradbarrows@peachjar.com>
2020-01-17 09:27:02 -0800

    script

--- generateAndThenPushItUp.sh
+++ generateAndThenPushItUp.sh
@@ -1,8 +1,2 @@
 bundle exec rake generate
-#bundle exec rake preview
-
-cp CNAME public/CNAME
-cp CNAME docs/CNAME
-git add *
-git commit -am "saving"
-git pull --rebase origin master; git push origin master
+echo "Created output into docs folder"
